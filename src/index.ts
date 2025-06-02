c20fe9142b87438ffd28d6554345e89c674eb6ef
Paul Hameteman <paul.hameteman@ns.nl>
2025-06-02 20:46:00 +0200

    T (red)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
+type Coordinates = {
+  row: number;
+  column: number;
+};
+
 export class TicTacToeGame {
+  private rows: number = 3;
+  private columns: number = 3;
+  private player: Player = Player.X;
+  private board: Cell[][] = new Array(this.rows)
+    .fill(Cell.EMPTY)
+    .map(() => new Array(this.columns).fill(Cell.EMPTY));
+
+  mark({ row, column }: Coordinates) {
+    this.board[row][column] = Cell.X;
+  }
+
   display(): string {
     return (
       `Game Board Creation...\n` +
@@ -12,4 +28,15 @@ export class TicTacToeGame {
       `The game will start with player X`
     );
   }
+}
+
+export enum Player {
+  X = `X`,
+  O = `O`,
+}
+
+export enum Cell {
+  EMPTY = ` `,
+  X = `X`,
+  O = `O`,
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -35,5 +35,4 @@ describe('Tic-Tac-Toe should', () => {
 
     expect(tictactoe.display()).toBe(expectedOutput);
   });
-
 });
